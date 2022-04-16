9d1d4bde999f9f41669daeac46ddbcf9b492fbea
A-Alekseev <ave.more@gmail.com>
2022-04-16 18:08:49 +0300

    HW1.Task2 - Создайте метод printThreeWords()

--- src/main/java/ru/gb/alekseev/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/alekseev/homework1/HomeWorkApp.java
@@ -2,6 +2,14 @@ package ru.gb.alekseev.homework1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        printThreeWords();
     }
+
+    //при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
 }
