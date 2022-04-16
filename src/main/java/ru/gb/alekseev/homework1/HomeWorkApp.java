89e5f2655880c2ea88adc8b4adb6239206eed3b6
A-Alekseev <ave.more@gmail.com>
2022-04-16 18:18:50 +0300

    HW1.Task3 - Создайте метод checkSumSign()

--- src/main/java/ru/gb/alekseev/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/alekseev/homework1/HomeWorkApp.java
@@ -3,6 +3,7 @@ package ru.gb.alekseev.homework1;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     //при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple
@@ -10,6 +11,18 @@ public class HomeWorkApp {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    //если сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”
+    public static void checkSumSign(){
+        int a = 15;
+        int b = -150;
+        if (a + b >= 0){
+            System.out.println("Сумма положительная");
+        }
+        else{
+            System.out.println("Сумма отрицательная");
+        }
     }
 
 }
