f22735e40712e6a7b5b7128e715e73f55a4b42ed
A-Alekseev <ave.more@gmail.com>
2022-06-06 01:35:08 +0300

    level 2 homework 3 - phonebook

--- src/main/java/ru/gb/alekseev/collections/Main.java
+++ src/main/java/ru/gb/alekseev/collections/Main.java
@@ -4,11 +4,31 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        UniqueWords();
+        uniqueWords();
 
+        System.out.println();
+        testPhonebook();
     }
 
-    private static void UniqueWords() {
+    //Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и
+    //телефонных номеров. В этот телефонный справочник с помощью метода add() можно
+    //добавлять записи, а с помощью метода get() искать номер телефона по фамилии. Следует
+    //учесть, что под одной фамилией может быть несколько телефонов
+    private static void testPhonebook() {
+        Phonebook pb = new Phonebook();
+        pb.add("Ivanov", "123-123");
+        pb.add("Petrov", "123-456-789");
+        pb.add("Sidorov", "123-123");
+        pb.add("Ivanov", "abc-def");
+
+        System.out.println(pb);
+    }
+
+
+    //Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и
+    //вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
+    //Посчитать, сколько раз встречается каждое слово
+    private static void uniqueWords() {
         String[] words = new String[]{
                 "Oh", "no", "not", "me",
                 "We", "never", "lost", "control",
@@ -19,8 +39,7 @@ public class Main {
 
         for (String word: words) {
             Integer oldCount = map.getOrDefault(word, 0);
-            map.put(word, oldCount+1);
-
+            map.put(word, oldCount + 1);
         }
 
         System.out.println(map.size() + " unique words of " + words.length + ":");
@@ -28,4 +47,7 @@ public class Main {
         System.out.println("Counts:");
         System.out.println(map);
     }
+
+
 }
+
